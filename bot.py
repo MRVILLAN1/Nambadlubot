60d6d4c99669bdbac95a964e55c9abb061b1cc60
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-07-13 14:55:58 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,15 @@
 # Telegram Channel @RknDeveloper & @Rkn_Bots
 # Developer @RknDeveloperr
 # Update Channel @Digital_Botz & @DigitalBotz_Support
-import os
-from hydrogram import Client, filters
-from config import Config
 
+import os, math, time, re
+from datetime import datetime
+from pytz import timezone
+from hydrogram import Client, filters
+from config import Config, rkn
+from hydrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 
 bot = Client(name="Tested-Botz",
             api_id=Config.API_ID,
@@ -17,6 +22,71 @@ bot = Client(name="Tested-Botz",
           #  plugins={"root": "plugins"},
             sleep_threshold=15)
 
+
+async def progress_for_pyrogram(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 5.00) == 0 or current == total:        
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "{0}{1}".format(
+            ''.join(["▣" for i in range(math.floor(percentage / 5))]),
+            ''.join(["▢" for i in range(20 - math.floor(percentage / 5))])
+        )            
+        tmp = progress + rkn.RKN_PROGRESS.format( 
+            round(percentage, 2),
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),            
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            await message.edit(
+                text=f"{ud_type}\n\n{tmp}",               
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])                                               
+            )
+        except:
+            pass
+
+def humanbytes(size):    
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'ʙ'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "ᴅ, ") if days else "") + \
+        ((str(hours) + "ʜ, ") if hours else "") + \
+        ((str(minutes) + "ᴍ, ") if minutes else "") + \
+        ((str(seconds) + "ꜱ, ") if seconds else "") + \
+        ((str(milliseconds) + "ᴍꜱ, ") if milliseconds else "")
+    return tmp[:-2] 
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+	
 @bot.on_message(filters.private & (filters.document | filters.audio | filters.video))
 async def download_media_test(bot, message):
     user_id = message.from_user.id
@@ -25,7 +95,7 @@ async def download_media_test(bot, message):
     try:
         # file downloading started...
         downloading = f"downloads/{user_id}/rkn{new_filename}"
-        path = await bot.download_media(message=file, file_name=downloading)#, progress=progress_for_pyrogram, progress_args=("ᴅᴏᴡɴʟᴏᴀᴅ sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
+        path = await bot.download_media(message=file, file_name=downloading, progress=progress_for_pyrogram, progress_args=("Download Started....", rkn_botz, time.time()))                    
     except Exception as e:
      	return await rkn_botz.edit(e)
      	     
@@ -33,8 +103,27 @@ async def download_media_test(bot, message):
     file_path = f"downloads/{user_id}/{new_filename}"
 	
     os.rename(path, file_path)
+    caption = f"**{new_filename}**"
+    ph_path = None
+    duration = 0
+    try:
+        parser = createParser(file_path)
+        metadata = extractMetadata(parser)
+        if metadata.has("duration"):
+            duration = metadata.get('duration').seconds
+        parser.close()
+    except:
+        pass
     await rkn_botz.edit('Try To Uploading')
-    await bot.send_video(video=file_path)
+    await bot.send_video(
+                    user_id,
+                    video=file_path,
+                    caption=caption,
+                    thumb=ph_path,
+                    duration=duration,
+                    progress=progress_for_pyrogram,
+                    progress_args=('Upload Started....', rkn_botz, time.time()))
+    
     await rkn_botz.edit('Your Files Successfully Uploaded')
     os.remove(file_path)
 
